8d05a620d9d53409591a4451f333081560f88ae6
Armando Correa <ie708826@iteso.mx>
2020-08-31 07:17:40 -0500

    Ejercicios integradores de clase3

---
+++ clase3/src/EjerciciosIntegradores.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import javax.swing.*;
+import java.io.*;
+
+public class EjerciciosIntegradores{
+
+    public static void main(String[] args){
+        /*EJERCICIO 1*/
+        // Scanner in = new Scanner(System.in);
+        // System.out.println("Introduce tu nombre: ");
+        // String name = in.nextLine();
+        // System.out.println("Introduce tu apellido paterno: ");
+        // String lastnameP = in.nextLine();
+        // System.out.println("Introduce tu apellido materno: ");
+        // String lastnameM = in.nextLine();
+        // System.out.println("Bienvenido: " + lastnameP.toUpperCase() + " " +
+        // lastnameM.toUpperCase() + "," + name.toUpperCase());
+
+        /*EJERCICIO 2*/
+        // Scanner scan = new Scanner(System.in);
+        // System.out.println("Velocidad Promedio (Km/h): ");
+        // int speed = scan.nextInt();
+        // System.out.println("Distancia a recorrer (Km): ");
+        // int distance = scan.nextInt();
+        // System.out.println("Tiempo en el que tiene que llegar (min): ");
+        // int time = scan.nextInt();
+        // int timeToDestination = (speed / distance) * 60;
+        //
+        // if (time < timeToDestination)
+        // {
+        //     System.out.println("NO llegaras al destino en tiempo");
+        // }
+        // else
+        // {
+        //     System.out.println("Llegaras al destino en tiempo");
+        // }
+
+
+        /*EJERCICIO 3*/
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("¿Cual es tu lenguaje de programacion favorito?");
+        // String language = sc.nextLine();
+        // language = language.toLowerCase();
+        // switch (language)
+        // {
+        //     case "java":
+        //         System.out.println("Su lenguaje es el mas popular");
+        //     break;
+        //
+        //     case "c":
+        //         System.out.println("Su lenguaje es el segundo mas popular");
+        //     break;
+        //
+        //     case "c++":
+        //         System.out.println("Su lenguaje es el tercero popular");
+        //     break;
+        //
+        //     default:
+        //         System.out.println("Su lenguaje no popular o no existe");
+        //     break;
+        // }
+    }
+
+}
